bd914b7f00f571b12f2b51d94415c71dd8e13481
ElsonNS <nselson26@gmail.com>
2024-10-24 16:27:39 +0530

    Updated API to use HTTPS for OMDb API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { useEffect, useState }  from 'react';
-import MovieCard from './MovieCard';
-import './App.css';
-import SearchIcon from './search.svg';
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import "./App.css";
+import SearchIcon from "./search.svg";
 //1861e18b
 
-const API_URL = "http://www.omdbapi.com?apikey=1861e18b"
+const API_URL = "https://www.omdbapi.com?apikey=1861e18b";
 
-// const movie1 ={ 
+// const movie1 ={
 //     "Title": "Superman, Spiderman or Batman",
 //     "Year": "2011",
 //     "imdbID": "tt2084949",
@@ -15,58 +15,50 @@ const API_URL = "http://www.omdbapi.com?apikey=1861e18b"
 // }
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const[movies, setMovies] = useState([]);
-  const[searchTerm, setSearchTerm] = useState('');
-
-  const searchMovie = async(title) =>{
-    const response = await fetch(`${API_URL}&s=${title}`)
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
     setMovies(data.Search);
+  };
 
-  }
-
-  useEffect(() =>{
-    searchMovie('Spiderman');
-  }, [])
+  useEffect(() => {
+    searchMovie("Spiderman");
+  }, []);
   return (
     <div className="app">
       <h1>MovieLand</h1>
 
-      <div className='search'>
-        <input placeholder='Search for movies'
+      <div className="search">
+        <input
+          placeholder="Search for movies"
           value={searchTerm}
-          onChange={(e) =>setSearchTerm(e.target.value)}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
 
-      <img
-        src = {SearchIcon}
-        alt ="search"
-        onClick ={() => searchMovie(searchTerm)}
-      />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovie(searchTerm)}
+        />
       </div>
 
-      {
-        movies?.length > 0
-        ? (
-            <div className='container'>
-              {movies.map((movie) => (
-                <MovieCard movie = {movie}  />
-              ))}
-            </div>
-          ) :
-          (
-            <div className='empty'>
-            <h2>No movies found</h2>
-            </div>
-
-          )
-      }
-      
-
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
